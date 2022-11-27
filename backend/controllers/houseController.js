e375910cfb69b7d202321041554fbf10a8d4ffb2
Yash Ranipa <ranipayash01@student.fdu.edu>
2022-11-26 16:40:05 -0800

    backend/ added middleware

--- backend/controllers/houseController.js
+++ backend/controllers/houseController.js
@@ -2,6 +2,11 @@
 // @route   GET /api/houses
 // @access  Public
 const getHouses = (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
+
   res.status(200).json({ message: "Get houses" });
 };
 
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/houses", require("./routes/houseRoutes"));
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started n port ${port}`));
