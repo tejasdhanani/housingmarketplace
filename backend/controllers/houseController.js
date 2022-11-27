e5d765616e5a88d7a5206a18612f94d5aa88da83
Yash Ranipa <ranipayash01@student.fdu.edu>
2022-11-26 19:41:23 -0800

    backend/ async handler setup

--- backend/controllers/houseController.js
+++ backend/controllers/houseController.js
@@ -1,35 +1,37 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc    Get houses
 // @route   GET /api/houses
 // @access  Public
-const getHouses = (req, res) => {
+const getHouses = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Get houses" });
+});
+
+// @desc    Set houses
+// @route   POST /api/houses
+// @access  Private
+const setHouse = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("Please add a text field");
   }
 
-  res.status(200).json({ message: "Get houses" });
-};
-
-// @desc    Set houses
-// @route   POST /api/houses
-// @access  Private
-const setHouse = (req, res) => {
   res.status(200).json({ message: "Set house" });
-};
+});
 
 // @desc    Update house
 // @route   PUT /api/houses/:id
 // @access  Private
-const updateHouse = (req, res) => {
+const updateHouse = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update house ${req.params.id}` });
-};
+});
 
 // @desc    Delete house
 // @route   DELETE /api/houses/:id
 // @access  Private
-const deleteHouse = (req, res) => {
+const deleteHouse = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete house ${req.params.id}` });
-};
+});
 
 module.exports = {
   getHouses,
