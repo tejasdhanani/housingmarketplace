180fc364c541c3800d5d761a4d06d6737e2f2d22
Yash Ranipa <ranipayash01@student.fdu.edu>
2022-12-03 14:41:26 -0800

    backend/ added getAllHouses in houseRoutes

--- backend/routes/houseRoutes.js
+++ backend/routes/houseRoutes.js
@@ -5,11 +5,13 @@ const {
   setHouse,
   updateHouse,
   deleteHouse,
+  getAllHouses,
 } = require("../controllers/houseController");
 
 const { protect } = require("../middleware/authMiddleware");
 
 router.route("/").get(protect, getHouses).post(protect, setHouse);
+router.route("/").get(getAllHouses);
 router.route("/:id").delete(protect, deleteHouse).put(protect, updateHouse);
 
 module.exports = router;
