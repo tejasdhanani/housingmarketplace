0f228a8066ac72e198a4ab08d8746657d5b53933
Yash Ranipa <ranipayash01@student.fdu.edu>
2022-12-03 20:06:57 -0800

    b/ applied multer to set house route

--- backend/routes/houseRoutes.js
+++ backend/routes/houseRoutes.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const multer = require("multer");
+const path = require("path");
 const router = express.Router();
+const fs = require("fs");
+
+const Storage = multer.memoryStorage();
+
+const upload = multer({ storage: Storage });
 const {
   getHouses,
   setHouse,
@@ -11,7 +18,10 @@ const {
 const { protect } = require("../middleware/authMiddleware");
 
 // Private
-router.route("/").get(protect, getHouses).post(protect, setHouse);
+router
+  .route("/")
+  .get(protect, getHouses)
+  .post(protect, upload.single("image"), setHouse);
 router.route("/:id").delete(protect, deleteHouse).put(protect, updateHouse);
 
 // Public
