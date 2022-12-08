a1726e65785bf02c6a58dba7557b2974138ad236
tejasdhanani <tejas27dhanani@gmail.com>
2022-12-08 11:05:57 -0800

    undo b/ serve frontend from backend

--- backend/server.js
+++ backend/server.js
@@ -16,22 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/houses", require("./routes/houseRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
-<<<<<<< HEAD
 // Undo server frontend
-=======
-// Serve frontend
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
-    )
-  );
-} else {
-  app.get("/", (req, res) => res.send("Please set to production"));
-}
->>>>>>> fb1e837454581b18f3118177a7d0634ef37badd6
 
 app.use(errorHandler);
 
