3d6a3ede7ecee57c8bf7c239e40402ca6b0c0635
tejasdhanani <tejas27dhanani@gmail.com>
2022-12-17 13:41:25 -0800

    f/ updated HouseItem to display city & rent

--- frontend/src/components/HouseItem.jsx
+++ frontend/src/components/HouseItem.jsx
@@ -1,30 +1,44 @@
 import { useDispatch } from "react-redux";
 import { deleteHouse } from "../features/houses/houseSlice";
+import { arrayBufferToBase64 } from "../functions/arrayBufferToBase64";
 
 function HouseItem({ house, canDelete }) {
   const dispatch = useDispatch();
 
-  const arrayBufferToBase64 = (buffer) => {
-    let binary = "";
-    let bytes = new Uint8Array(buffer);
-    let len = bytes.byteLength;
-    for (let i = 0; i < len; i++) {
-      binary += String.fromCharCode(bytes[i]);
-    }
-    return window.btoa(binary);
-  };
+  const {
+    title,
+    image,
+    city,
+    postalCode,
+    description,
+    rent,
+    sqFt,
+    type,
+    laundry,
+    bedroom,
+    bathroom,
+    petsAllowed,
+    canSmoke,
+    isFurnished,
+    lease,
+    walkthroughVideo,
+  } = house;
+
+  console.log(house);
 
   return (
     <div className="house">
-      <div>{new Date(house.createdAt).toLocaleString("en-US")}</div>
-      <h2>{house.text}</h2>
-      <img
-        src={
-          "data:image/jpeg;base64," + arrayBufferToBase64(house.file.data.data)
-        }
-        width={"350px"}
-        alt="sdfsdf"
-      />
+      {/* <div>{new Date(house.createdAt).toLocaleString("en-US")}</div> */}
+      <h2>{title}</h2>
+      {image && (
+        <img
+          src={"data:image/jpeg;base64," + arrayBufferToBase64(image.data.data)}
+          width={"350px"}
+          alt="sdfsdf"
+        />
+      )}
+      <h1>Rent: ${rent}</h1>
+      <h3>{city}</h3>
       {canDelete ? (
         <button
           onClick={() => dispatch(deleteHouse(house._id))}
